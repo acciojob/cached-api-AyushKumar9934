fc9923f133924cf129cb9d36a1534100d9baaeee
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-06-29 15:58:59 +0530

    f

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data,setData]=useState(null);
+    const [loading,SetLoading]=useState(true);
+   
+  
+  useEffect(()=>{
+    const fetchData=async()=>{
+    const res=await fetch("https://jsonplaceholder.typicode.com/posts");
+   const data=await res.json();
+   setData(data);
+   SetLoading(false);}
+   fetchData();
+  
+  },[])
+  const memoisedData=useMemo(()=>data,[data]);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>OUTPUT:</h1><br />
+      {
+        loading?(<p>Loading...</p>):memoisedData.map((item)=>{
+        return <><b>{item.title}</b>
+          <p>{item.body}</p></> 
+        })
+      }
+      
+  
+
     </div>
   )
 }
